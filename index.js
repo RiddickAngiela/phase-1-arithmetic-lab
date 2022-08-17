fd7229355126bf9631affa539b9a7d0daa9caffe
Riddick <riddick.angiela@student.moringaschool.com>
2022-08-17 13:36:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-// Write your code here
+let num1=2 
+let num2=31
+let multiply = num1*num2
+let random= Math.floor(Math.random()*100)
+console.log(random)
+let num3=9
+let num4=5
+let mod= num3%num4
+let max=Math.max(1,4,6,9,20)
+console.log(max)
